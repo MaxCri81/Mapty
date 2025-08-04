b106f2e3e28a6a2c9875673bec5ec8915e9b25c2
Massimiliano Crisafulli <maxone981@gmail.com>
2025-08-04 15:43:09 +0100

    Update script.js
    add a customized popup when clicking on the map

--- script.js
+++ script.js
@@ -11,6 +11,15 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+// Leaflet popup object
+const leafletObj = {
+    maxWidth: 250,
+    minWidth: 100,
+    autoClose: false,
+    closeOnClick: false,
+    className: "running-popup",
+};
+
 // Geolocation coordinates
 if (navigator.geolocation) navigator.geolocation.getCurrentPosition(success, error);
 
@@ -28,18 +37,41 @@ function success(position) {
     // Leaflet library
     const map = L.map('map').setView(coordinates, 13); // 13: zoom level
 
-    // OpenStreetMap.HOT style
+    // OpenStreetMap.HOT map style
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
     }).addTo(map);
 
-    L.marker(coordinates).addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+    // Attach a Leaflet event listener to the map object
+    // map.on("click", (mapEvent)=> {
+    //     console.log(mapEvent);
+        
+    //     const {lat, lng} = mapEvent.latlng;
+
+    //     // Add a marker when clicking on the map
+    //     L.marker([lat, lng])
+    //         .addTo(map) // add the marker
+    //         .bindPopup(L.popup(leafletObj)) // on clicking to the map binds leafletObj 
+    //         .setPopupContent("Workout") // set the popup content
+    //         .openPopup();
+    // });
+    map.on("click", leafletMarker.bind(map));
 };
 
 /** Callback function invoked upon failing retrieving geolocation coordinates. */
 function error(){
     alert("Could not get your position");
+};
+
+
+function leafletMarker(mapEvent) {
+    const {lat, lng} = mapEvent.latlng;
+  
+    // Add a marker when clicking on the map
+    L.marker([lat, lng])
+        .addTo(this) // add the marker to the map (I binded the map to "this")
+        .bindPopup(L.popup(leafletObj)) // on clicking to the map binds leafletObj 
+        .setPopupContent("Workout") // set the popup content
+        .openPopup();
 };
